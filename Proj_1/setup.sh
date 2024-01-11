c043af9b3cc1031cf6e068aaae2bd8506d08659f
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2024-01-10 23:39:33 -0600

    Update setup.sh

--- Proj_1/setup.sh
+++ Proj_1/setup.sh
@@ -4,4 +4,63 @@
 ### Proj_1 Script ###
 #####################
 
-#
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
+
+# Installs tools
+sudo apt update
+sudo apt install -y members net-tools vim sl expect
+
+#Create a File System on /dev/sd{b,c}
+sudo mkfs.ext4 /dev/sdb
+sudo mkfs.ext4 /dev/sdc
+
+# Changing the mount point of /var/www to /dev/sdb
+sudo mkdir /mnt/tmp
+sudo mount /dev/sdb /mnt/tmp
+sudo rsync -avx /var/www/* /mnt/tmp/
+sudo umount /mnt/tmp
+sudo mount /dev/sdb /var/www
+
+# Aquire the UUID for /dev/sdb
+result=$(sudo blkid -o value -s UUID "/dev/sdb")
+
+# Print the UUID to check if it's obtained correctly
+echo "UUID: $result"
+
+# Define the line to check for in fstab
+desired_line1="UUID=$result  /var/www ext4  defaults  0 2"
+
+# Check if the line already exists in the fstab file
+if grep -Fxq "$desired_line1" /etc/fstab; then
+  echo "The line is already present in /etc/fstab."
+else
+  # Add the line to fstab using echo and append (>>) operator
+  echo "$desired_line1" | sudo tee -a /etc/fstab > /dev/null
+  echo "The line has been added to /etc/fstab."
+fi
+
+# Changing the mount point of /backups to /dev/sdc
+sudo mkdir -p /backups/web-servers
+sudo mount /dev/sdb /backups
+
+# Aquire the UUID for /dev/sdc
+result=$(sudo blkid -o value -s UUID "/dev/sdc")
+
+# Print the UUID to check if it's obtained correctly
+echo "UUID: $result"
+
+# Define the line to check for in fstab
+desired_line1="UUID=$result  /backups ext4  defaults  0 2"
+
+# Check if the line already exists in the fstab file
+if grep -Fxq "$desired_line1" /etc/fstab; then
+  echo "The line is already present in /etc/fstab."
+else
+  # Add the line to fstab using echo and append (>>) operator
+  echo "$desired_line1" | sudo tee -a /etc/fstab > /dev/null
+  echo "The line has been added to /etc/fstab."
+fi
