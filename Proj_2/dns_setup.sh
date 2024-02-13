622109f646b7bdbfc070456a4040add55fc4e8c9
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2024-02-12 23:09:10 -0600

    Update dns_setup.sh

--- Proj_2/dns_setup.sh
+++ Proj_2/dns_setup.sh
@@ -29,3 +29,29 @@ EOF
 # Creating directories and moving files
 sudo mkdir -p /backups/dns-server/{full_backup,diff_backup}
 sudo mkdir /backups/dns-server/full_backup/{new,old}
+sudo scp -f /etc/scripts/CSC271/Proj_2/named.conf.options /etc/bind/named.conf.options
+sudo scp -f /etc/scripts/CSC271/Proj_2/named.conf.local /etc/bind/named.conf.local
+sudo scp -f /etc/scripts/CSC271/Proj_2/default.named /etc/default/named
+sudo mv /etc/scripts/CSC271/Proj_2/forward.boofblasters.com /etc/bind/forward.boofblasters.com
+sudo mv /etc/scripts/CSC271/Proj_2/reverse.boofblasters.com /etc/bind/reverse.boofblasters.com
+
+# Start/Enable the named service and check configs
+sudo systemctl start named && sudo systemctl enable named
+
+if sudo named-checkconf /etc/bind/named.conf.local; then
+  echo "Configuration file is OK."
+else
+  echo "Error: Configuration file syntax"
+fi
+
+if sudo named-checkzone boofblasters.com /etc/bind/forward.boofblasters.com; then
+  echo "Forward zones are OK"
+else
+  echo "Forward zone syntax error"
+fi
+
+if sudo named-checkzone boofblasters.com /etc/bind/reverse.boofblasters.com; then
+  echo "Reverse zones are OK"
+else
+  echo "Reverse zone syntax error"
+fi
