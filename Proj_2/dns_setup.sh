6cd7e63c7b31007ce425434c1569c125c64ea306
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2024-02-12 21:51:42 -0600

    Update dns_setup.sh

--- Proj_2/dns_setup.sh
+++ Proj_2/dns_setup.sh
@@ -3,5 +3,25 @@
 ### Proj_2 Script ###
 #####################
 
-# Make the user
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
 
+# Creating users
+sudo adduser --disabled-password --gecos "" dnseditor
+
+expect << EOF
+set NEW_PASSWORD "changeme"   ;# Replace with the desired new password
+set USERNAME_LIST {dnseditor}
+
+foreach username \$USERNAME_LIST {
+    spawn passwd \$username
+    expect "New password: "
+    send "\$NEW_PASSWORD\r"
+    expect "Retype new password: "
+    send "\$NEW_PASSWORD\r"
+    expect eof                    ;
+}
+EOF
